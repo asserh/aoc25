7fcab2dde83848792c079bf9bf696e30e1643711
Asser Hakala <asserhakala@gmail.com>
2025-12-22 15:00:10 +0100

    wip: day1 part 2

--- Sources/Aoc25/Days/Day1.swift
+++ Sources/Aoc25/Days/Day1.swift
@@ -4,7 +4,7 @@ struct Day1: Day {
   func part1() -> String {
     var dial = 50
     var count = 0
-    
+
     for line in input {
       guard let number = Int(line.dropFirst()) else {
         fatalError("invalid line")
@@ -15,16 +15,43 @@ struct Day1: Day {
       } else {
         dial += number
       }
-      
+
       if dial % 100 == 0 {
         count += 1
       }
     }
-    
+
     return String(count)
   }
 
   func part2() -> String {
-    return ""
+    var dial = 50
+    var count = 0
+
+    for line in input {
+      guard let number = Int(line.dropFirst()) else {
+        fatalError("invalid line")
+      }
+
+      let result: Int
+      if line.hasPrefix("L") {
+        result = (dial - number) - 100
+      } else {
+        result = dial + number
+      }
+
+      dial = ((result % 100) + 100) % 100
+
+      print(dial)
+      if dial == 0 {
+        count += 1
+      } else if dial + number > 100 {
+        let c = abs(result) / 100
+        print("Counting on \(c) \(line)")
+        count += c
+      }
+    }
+
+    return String(count)
   }
 }
--- Tests/Aoc25Tests/Day1Tests.swift
+++ Tests/Aoc25Tests/Day1Tests.swift
@@ -1,13 +1,19 @@
 import Testing
 
-@testable import Aoc2025
+@testable import Aoc25
 
 @Suite("Day 1")
 struct Day1Tests {
-
+  
   @Test("Part 1 should equal 3")
   func part1() {
     let day = Day1(sample: true)
     #expect(day.part1() == "3")
+  }
+  
+  @Test("Part 2 should equal 26")
+  func part2() {
+    let day = Day1(sample: true)
+    #expect(day.part2() == "26")
   }
 }
